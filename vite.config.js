515d0a640e91b7761ce08b59c1db5c6557fb0c3d
Chema Torres <chematorres@Chemas-MacBook-Pro.local>
2025-08-16 17:45:44 +0200

    chore: preparar build de producción

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/',           // si tu app estará en subcarpeta, cámbialo
+  build: { target: 'es2018', sourcemap: false, emptyOutDir: true, outDir: 'dist' }
+});
